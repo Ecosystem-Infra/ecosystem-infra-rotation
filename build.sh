8a13caf321ecc313f1c624b257860633b949329f
Philip Jägenstedt <philip@foolip.org>
2019-05-20 15:30:36 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,5 @@ set -o pipefail
 # Install webapp dependencies.
 cd webapp
 rm -rf bower_components
-# This will call `bower install`. `--unsafe-perm` is needed for GitHub Actions where
-# this script will be run as root.
-npm install --unsafe-perm
+# This will call `bower install`.
+npm install
