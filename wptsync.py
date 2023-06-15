63ceaacf2dc36b26528ce80c91ad63bda71599b6
Jonathan Lee <jonathanjlee@google.com>
2023-06-15 16:52:37 -0400

    Find import/export jobs via Buildbucket RPC
    The builder page was recently redesigned, which broke the `wptsync`
    scraper. This causes `/api/wptsync/*` to never return any builds, so the
    tasks on the dashboard are identically green.
    
    This PR updates the API handler to fetch import/export job data via
    Buildbucket RPC, which is designed for machine consumption and therefore
    more robust. There should be no functional change to `/apt/wptsync/*`.
    
    This also obviates the need for MechanicalSoup for scraping, so that
    dependency has been removed.

--- wptsync.py
+++ wptsync.py
@@ -1,29 +1,82 @@
-import mechanicalsoup
+import json
+from datetime import datetime
+from typing import TypedDict, List
 
-BASE_URL = 'https://ci.chromium.org'
-IMPORT_URL = BASE_URL + '/p/infra/builders/luci.infra.cron/wpt-importer?limit=50'
-EXPORT_URL = BASE_URL + '/p/infra/builders/luci.infra.cron/wpt-exporter?limit=50'
+import requests
+
+BUILDBUCKET_HOST = 'cr-buildbucket.appspot.com'
+BUILDBUCKET_API = 'buildbucket.v2.Builds'
+PRPC_PREFIX = ")]}'"  # Anti-XSS escape before every response
+PROJECT, BUCKET = 'infra', 'cron'
+BUILD_FIELDS = [
+    'createTime',
+    'startTime',
+    'endTime',
+    'status',
+    'id',
+    'summaryMarkdown',
+]
 
 
-def scrape_buildbot(url):
-    browser = mechanicalsoup.StatefulBrowser()
-    browser.open(url)
-    rows = browser.get_current_page().select('div.main table.info tr')
+class Build(TypedDict):
+    time: str
+    duration: str
+    result: str
+    build: str
+    info: str
 
-    builds = []
-    recent_failures = 0
-    last_success = None
-    # Skip the table header.
-    for row in rows[1:]:
-        columns = row.select('td')
 
-        time = columns[0].get_text().strip()
-        # columns[1] is end time
-        duration = columns[2].get_text().strip()
-        # columns[3] is commit
-        result = columns[4].get_text().strip()
-        build = BASE_URL + columns[5].a['href']
-        info = columns[6].get_text().strip()
+class Results(TypedDict):
+    last_success: str
+    recent_failures: int
+    builds: List[Build]
+
+
+def search_builds(builder_name: str, count: int = 50) -> Results:
+    """Fetch the latest completed builds for an `infra/cron` builder.
+
+    Arguments:
+        builder_name: Builder to fetch builds for.
+        count: Number of latest builds to fetch.
+
+    See Also:
+        https://source.chromium.org/chromium/_/chromium/infra/luci/luci-go/+/1fbb74377bfb72e37328c737c66eabb1521d8c1f:buildbucket/proto/builds_service.proto;l=175-216
+    """
+    rpc_url = f'https://{BUILDBUCKET_HOST}/prpc/{BUILDBUCKET_API}/SearchBuilds'
+    request_payload = {
+        'predicate': {
+            'builder': {
+                'project': PROJECT,
+                'bucket': BUCKET,
+                'builder': builder_name,
+            },
+            # Only fetch completed jobs.
+            'status': 'ENDED_MASK',
+        },
+        'fields': ','.join(f'builds.*.{field}' for field in BUILD_FIELDS),
+        'pageSize': count,
+    }
+    response = requests.post(rpc_url, json=request_payload, headers={
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Accept-Encoding': 'gzip',
+    })
+    response.raise_for_status()
+    response_payload = json.loads(
+        response.text.removeprefix(PRPC_PREFIX).strip())
+
+    last_success, recent_failures, builds = None, 0, []
+    # Buildbucket already returns builds newest to oldest.
+    for raw_build in response_payload['builds']:
+        time = raw_build['createTime']
+        start = datetime.fromisoformat(raw_build['startTime'])
+        end = datetime.fromisoformat(raw_build['endTime'])
+        duration = _format_duration((end - start).total_seconds())
+        result = raw_build['status']
+        build = (f'https://ci.chromium.org/p/{PROJECT}/builders'
+                 f'/{BUCKET}/{builder_name}/b{raw_build["id"]}')
+        info = raw_build.get('summaryMarkdown', '')
+
         if result == 'SUCCESS':
             last_success = last_success or time
         else:
@@ -35,7 +88,7 @@ def scrape_buildbot(url):
             'duration': duration,
             'result': result,
             'build': build,
-            'info': info
+            'info': info,
         })
 
     result = {
@@ -47,9 +100,18 @@ def scrape_buildbot(url):
     return result
 
 
+def _format_duration(seconds: float) -> str:
+    minutes, seconds = divmod(max(0, seconds), 60)
+    hours, minutes = divmod(minutes, 60)
+    hours, minutes, seconds = int(hours), int(minutes), int(seconds)
+    if hours:
+        return f'{hours} hrs {minutes} mins'
+    return f'{minutes} mins {seconds} secs'
+
+
 def import_status():
-    return scrape_buildbot(IMPORT_URL)
+    return search_builds('wpt-importer')
 
 
 def export_status():
-    return scrape_buildbot(EXPORT_URL)
+    return search_builds('wpt-exporter')
